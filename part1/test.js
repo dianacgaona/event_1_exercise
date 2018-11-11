b9a84e77460e99e341e3566c897de59b43a34772
DianaG <dianagaona@pursuit.org>
2018-11-10 21:19:38 -0500

    Diana Gaona

---
+++ part1/test.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  debugger;
+  window.alert("Your page has loaded!");
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('The page has loaded');
+//   debugger;
+// });
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   let button = document.querySelector(".touchMe");
+//   button.addEventListener("click", event => {
+//     button.style.background = "green";
+//   });
+// });
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   let selector = document.querySelector('#coords');
+//   window.addEventListener('mousemove', event => {
+//     //document. or window.
+//     coords.innerHTML = `coordX: ${event.offsetX} coordY: ${event.offsetY}`;
+//     // xpoint.innerText = event.clientX;
+//     // ypoint.innerText = event.clientY;
+//   });
+// });
